bf5c618c0793a88374e360a77e22890aed1ae207
keisokoo <keisokoo@gmail.com>
2022-12-10 17:18:17 +0900

    feat, 이미지 미리보기 로직

--- src/pages/popup/components/Tooltip/Tooltip.styles.tsx
+++ src/pages/popup/components/Tooltip/Tooltip.styles.tsx
@@ -1,8 +1,15 @@
 import styled from '@emotion/styled/macro'
+import { colors, typography } from '../../themes/styles'
 import { addCssProps } from '../../themes/styles.helper'
 
 const TooltipStyle = {
   Wrap: styled.div`
+    background-color: ${colors['Warning/Light']};
+    ${typography['Body/Caption/Bold']}
+    color: ${colors['Grayscale/Gray Dark']};
+    transform: translateY(calc(100% + 6px));
+    padding: 6px 12px;
+    position: absolute;
     ${addCssProps}
   `,
 }
--- src/pages/popup/components/Tooltip/index.tsx
+++ src/pages/popup/components/Tooltip/index.tsx
@@ -1,10 +1,14 @@
 import TooltipStyle from './Tooltip.styles'
 import { TooltipProps } from './Tooltip.types'
 
-const Tooltip = ({ _css, ...props }: TooltipProps) => {
+const Tooltip = ({ _css, children, ...props }: TooltipProps) => {
   return (
     <>
-      <TooltipStyle.Wrap _css={_css} {...props}></TooltipStyle.Wrap>
+      {children && (
+        <TooltipStyle.Wrap _css={_css} {...props}>
+          {children}
+        </TooltipStyle.Wrap>
+      )}
     </>
   )
 }
